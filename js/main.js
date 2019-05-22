5835d3834f8e786e877aa25b5d45b682b1626d88
Bernardo Ferreira <bernardoferreira12@hotmail.com>
2019-05-22 02:27:47 +0100

    Add changes

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// /*SITCKY HEADER*/
+// // When the user scrolls the page, execute stickyTop 
+// window.onscroll = function() {stickyTop()};
+
+// // Get the header
+// const header = document.getElementById('header');
+
+// // Get the offset position of the navbar
+// const sticky = header.offsetTop;
+
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function stickyTop() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add('sticky');
+//   } else {
+//     header.classList.remove('sticky');
+//   }
+// }
