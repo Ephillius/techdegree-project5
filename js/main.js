a71508103f1489a67a3d04e151cdc1c602235de9
Bernardo Ferreira <bernardoferreira12@hotmail.com>
2019-05-24 21:08:39 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-$('image_captions') = [
+// stores all captions used, in order of placement, of course
+var captions = [
 	'I love hay bales. Took this snap on a drive through the countryside past some straw fields.',
 	'The lake was so calm today. We had a great view of the snow on the mountains from here.',
 	'I hiked to the top of the mountain and got this picture of the canyon and trees below.',
-	'It was amazing to see an iceberg up close, it was so cold but didn’t snow today.' ,
+	'It was amazing to see an iceberg up close, it was so cold but didn&apos;t snow today.' ,
 	'The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons.',
 	'Fall is coming, I love when the leaves on the trees start to change color.',
 	'I drove past this plantation yesterday, everything is so green!',
@@ -13,9 +14,30 @@ $('image_captions') = [
 	'I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in.'
 ]
 
-const captions = $('image_captions')
+//This function simplifies my job in terms of attribute addition
+//I don't need to add the captions to each one of the photos manually
+// when the page loads
+$(document).ready(function(){
+	//for each anchor tag child do:
+	$(".grid_image a").each(function(index){
+		//1st, if they have an attribute that starts with 'photo'
+		if($('a[href^="photo"]')){
+			//add another attribute of 'data-lightbox' with the value of 'roadtrip'
+			//this will initialize the lightbox plugin on the specified anchor tags
+			$(this).attr('data-lightbox','roadtrip');
+			// just checking if it works here
+			console.log("Attribute added");
+			//another attribute will be added to the anchor tag
+			//this time, the function will iterate through the captions array
+			//and add the plugin attribute + corresponding caption
+			$(this).attr('data-title', captions[index]);
+			console.log(captions[index] + ' - caption added'); 
+		}
+	})
+});
 
-for (let i; i < captions.length; i++) {
-	$('a[href^="photos"]').attr('data-lightbox','roadtrip');
-	$('a[href^="photos"]').attr('data-title', 'captions[i]');
-}
+//Lightbox options
+lightbox.option({
+	'alwaysShowNavOnTouchDevices': true,
+	'positionFromTop': 20
+})
